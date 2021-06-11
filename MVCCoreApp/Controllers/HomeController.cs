02a8c92ec2de3348a45f73e1c92e5e3bacb274e6
doanle123456789 <doanngocle17012016@gmail.com>
2021-06-12 00:22:09 +0700

    Add project files.

---
+++ MVCCoreApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCoreApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "heloolknklnlk";
+        }
+    }
+}
